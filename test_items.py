3d7040f8b6841f48eef2d9bea7ff75ee45ce9d46
VolodymyrSurivka <56838756+VolodymyrSurivka@users.noreply.github.com>
2020-09-14 00:30:00 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,10 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
 
 def test_guest_should_see_add_to_basket_button(browser):
-    browser.get(link)
-    time.sleep(30)
-    add_to_basket_button = browser.find_element_by_css_selector("button.btn-add-to-basket").click()
-    assert len(str(add_to_basket_button)) > 0, "The \"Add to the basket\" button isn't on this page"
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(10)
+    add_to_basket_button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert add_to_basket_button, "The \"Add to the basket\" button isn't on this page"
+    add_to_basket_button.click()
     time.sleep(3)
